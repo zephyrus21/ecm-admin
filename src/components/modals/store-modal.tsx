0ff5e3c51fe6edd20abc587adc4116f1ff85ebe6
Piyush Pandey <pandey.piyus@gmail.com>
2023-07-12 17:17:54 +0530

    added modals

--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import Modal from '@/components/modals/modal';
 import { useStoreModal } from '@/hooks/use-store-modal';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  name: z.string().nonempty().min(1),
+});
 
 const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
 
   return (
     <Modal
@@ -11,7 +40,33 @@ const StoreModal = () => {
       description='Create a store to manage products'
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}>
-      Create Store
+      <div>
+        <div className='space-y-4 py-2 pb-4'>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                name='name'
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder='Ecm' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
+                <Button variant={'outline'} onClick={storeModal.onClose}>
+                  Cancel
+                </Button>
+                <Button type='submit'>Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
