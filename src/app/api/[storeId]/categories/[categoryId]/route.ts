682e343be0e16ee9bd497021d59b59b6a168d4cd
Piyush Pandey <pandey.piyus@gmail.com>
2023-07-18 15:42:17 +0530

    fixed: bugs

--- src/app/api/[storeId]/categories/[categoryId]/route.ts
+++ src/app/api/[storeId]/categories/[categoryId]/route.ts
@@ -15,6 +15,9 @@ export async function GET(
       where: {
         id: categoryId,
       },
+      include: {
+        billboard: true,
+      },
     });
 
     return NextResponse.json(category);
